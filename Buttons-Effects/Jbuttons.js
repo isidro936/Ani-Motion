92a7328026616d26fb8b0b840a4f58b1498fc227
Isidro Ernesto T <isidroernestodeltorrentegonzal@gmail.com>
2023-08-23 17:32:04 -0400

    v1.1

--- Buttons-Effects/Jbuttons.js
+++ Buttons-Effects/Jbuttons.js
@@ -1,9 +1,11 @@
 export function eventButtons(props) {  
     props.element.addEventListener(props.event, ()=>{
-        props.element.classList.add(props.effect)
+        props.element.classList.toggle(props.effect)
     })
 }
 
 export const effects = {
     loading: 'Jbtn-loading',
+    scale: 'Jbtn-scale',
+    shadow: 'Jbtn-shadow'
 }
---
+++ Img-Effects/imgEff.js
@@ -0,0 +1,9 @@
+export const imgScroll = function(props){
+    window.addEventListener("scroll",() => {
+        const scrollTop = window.scrollY;
+        const scale = 1 + scrollTop * props.units;
+        const limitedScale = Math.min(props.maxScale, Math.max(props.minScale, scale));
+        props.element.style.transform = `scale(${limitedScale})`;
+    });
+}
+
